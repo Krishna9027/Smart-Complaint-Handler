2ceb4e4b46685fd53e4166788e736f9cfcc55076
Krishna Gupta <krishnaguptamnnit45@gmail.com>
2025-09-15 01:16:57 +0530

    Update Login.jsx

--- frontend/src/pages/Authentication/Login.jsx
+++ frontend/src/pages/Authentication/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post("http://localhost:3000/api/v1/auth/login", {
+      const response = await axios.post("https://smart-complaint-handler.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
